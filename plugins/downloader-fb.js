c630f6031872a1cbd87a9a4b41a8f7cc462abf47
White Shadow Ofc <kaweeshachamod00@gmail.com>
2024-03-10 19:05:43 +0530

    Update downloader-fb.js

--- plugins/downloader-fb.js
+++ plugins/downloader-fb.js
@@ -1,22 +1,35 @@
-let handler = async(m, { conn, text, usedPrefix, command }) => {
-	if (!text) throw `*Usage : ${usedPrefix + command} fb_url_video*\n\nExample :\n${usedPrefix + command} `
-	if (!(text.includes('http://') || text.includes('https://'))) throw `url invalid, please input a valid url. Try with add http:// or https://`
-	try {
-		let anu = await fetch(`https://api-rest-jessi2devolop.koyeb.app/api/dowloader/fbdown?url=${text}`)
-		let json = await anu.json()
-		let txt = `${json.hasil.title}`
-		await conn.sendMsg(m.chat, { video: { url: json.hasil.watermark }, caption: json.hasil.title }, { quoted: m })
-	} catch (e) {
-		console.log(e)
-		m.reply(`Invalid facebook url.`)
-	}
+import fetch from 'node-fetch';
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!/https?:\/\/)?(?:www\.)?(?:facebook\.com|fb\.watch)\([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i.test(args[0])) {
+        await m.react('🤨');
+        return m.reply(`Use example ${usedPrefix}${command} link`);
+    }
+    
+    await m.react('🕥');
+    
+    try {
+        let response = await fetch(`https://api-rest-jessi2devolop.koyeb.app/api/dowloader/fbdown?url=${args[0]}`);
+        let data = await response.json();
+    await m.react('🕚');
+        
+        if (data.status && data.result) {
+          
+            await conn.sendMessage(m.chat, { video: { url: data.result[0].url } }, { quoted: m }); 
+            return m.react('✅');
+        } else {
+            throw new Error('Error in response data');
+        }
+    } catch (error) {
+        console.error('Error:', error.message);
+        m.reply('Failed to fetch Instagram data. Please try again later.');
+        await m.react('😑');
+    }
 }
 
-handler.menudownload = ['fb <url>']
-handler.tagsdownload = ['search']
-handler.command = /^(facebook(fb)?)$/i
+handler.help = ['insta'].map(v => v + ' <url>')
+handler.tags = ['downloader']
 
-handler.premium = true
-handler.limit = true
+handler.command = /^(fb(dl)?|fb|facebook(dl)?)$/i
 
-export default handler
+export default handler;
