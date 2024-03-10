4dfb5e3c9d0b35f7384ee67bef9ec775796333c3
White Shadow Ofc <kaweeshachamod00@gmail.com>
2024-03-10 19:12:16 +0530

    Update downloader-fb.js

--- plugins/downloader-fb.js
+++ plugins/downloader-fb.js
@@ -1,8 +1,8 @@
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!/https?:\/\/)?(?:www\.)?(?:facebook\.com|fb\.watch)\([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i.test(args[0])) {
-        await m.react('🤨');
+    if (!text) {
+        await m.react('🫡');
         return m.reply(`Use example ${usedPrefix}${command} link`);
     }
     
@@ -15,21 +15,20 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
         
         if (data.status && data.result) {
           
-            await conn.sendMessage(m.chat, { video: { url: data.result[0].url } }, { quoted: m }); 
+            await conn.sendMessage(m.chat, { video: { url: data.result.Normal_video } }, { quoted: m }); 
             return m.react('✅');
         } else {
             throw new Error('Error in response data');
         }
     } catch (error) {
         console.error('Error:', error.message);
-        m.reply('Failed to fetch Instagram data. Please try again later.');
+        m.reply('Failed to fetch fb data. Please try again later.');
         await m.react('😑');
     }
 }
 
-handler.help = ['insta'].map(v => v + ' <url>')
+handler.help = ['facebook <url>']
 handler.tags = ['downloader']
-
-handler.command = /^(fb(dl)?|fb|facebook(dl)?)$/i
+handler.command = /^((facebook|fb)(downloder|dl)?)$/i
 
 export default handler;
