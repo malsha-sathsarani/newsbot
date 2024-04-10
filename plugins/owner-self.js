a7e8d499a2dfb90026a24bb562956eb603eb176b
White Shadow Ofc <kaweeshachamod00@gmail.com>
2024-04-10 14:06:09 +0530

    Update owner-self.js

--- plugins/owner-self.js
+++ plugins/owner-self.js
@@ -1,19 +1,19 @@
-let handler = async(m, { conn, command }) => {
-  let isPublic = command === "public";
-  let self = global.opts["self"]
+//let handler = async(m, { conn, command }) => {
+  //let isPublic = command === "public";
+  //let self = global.opts["self"]
 
-  if(self === !isPublic) return m.reply(`Dah ${!isPublic ? "Self" : "Public"} dari tadi ${m.sender.split("@")[0] === global.owner[1] ? "Mbak" : "Bang"} :v`)
+  //if(self === !isPublic) return m.reply(`Dah ${!isPublic ? "Self" : "Public"} dari tadi ${m.sender.split("@")[0] === global.owner[1] ? "Mbak" : "Bang"} :v`)
 
-  global.opts["self"] = !isPublic
+  //global.opts["self"] = !isPublic
 
-  m.reply(`Berhasil ${!isPublic ? "Self" : "Public"} bot!`)
-}
+  //m.reply(`Berhasil ${!isPublic ? "Self" : "Public"} bot!`)
+//}
 
-handler.help = ["self", "public"]
-handler.tags = ["owner"]
+//handler.help = ["self", "public"]
+//handler.tags = ["owner"]
 
-handler.rowner = true
+//handler.rowner = true
 
-handler.command = /^(self|public)/i
+//handler.command = /^(self|public)/i
 
-export default handler 
+//export default handler 
