9dbe4908a552f1809c68d13b6d3b8bb5e2fc70aa
White Shadow Ofc <kaweeshachamod00@gmail.com>
2024-04-23 08:03:32 +0530

    Update tool-translate.js

--- plugins/tool-translate.js
+++ plugins/tool-translate.js
@@ -1,30 +1,38 @@
-import translate from '@vitalets/google-translate-api'
-import fetch from 'node-fetch'
-let handler = async (m, { args, usedPrefix, command }) => {
-let msg = `*This enables you to translate instantly into all languages* 
-_Example if you want to translate into Sinhala_ : 
-*.tr* si Hello`
-if (!args || !args[0]) return m.reply(msg)  
-let lang = args[0]
-let text = args.slice(1).join(' ')
+
+import {translate} from '@vitalets/google-translate-api'
 const defaultLang = 'en'
-if ((args[0] || '').length !== 2) {
-lang = defaultLang
-text = args.join(' ')}
-if (!text && m.quoted && m.quoted.text) text = m.quoted.text
-try {      
-let result = await translate(`${text}`, { to: lang, autoCorrect: true })
-await m.reply('━━😄 Hello my friend, follow me on Instagram \n instagram.com/mr.whiteshadowofc\n\n\n\n' + result.text)
-} catch {
-try {    
-let lol = await fetch(`https://api.lolhuman.xyz/api/translate/auto/${lang}?apikey=85faf717d0545d14074659ad&text=${text}`)
-let loll = await lol.json()
-let result2 = loll.result.translated
-await m.reply('*Here is your translation, \:* ' + result2)
-} catch { 
-await m.reply('*Problem, try again later*')    
-}}}
-handler.help = ["translate"]
-handler.tags = ["tools"]
-handler.command = /^(translate|tr)$/i
+const tld = 'cn'
+
+let handler = async (m, { args, usedPrefix, command }) => {
+    let err = `
+📌 *Example:*
+
+*${usedPrefix + command}* <id> [text]
+*${usedPrefix + command}* en Hello World
+
+≡ *List of supported languages:* 
+
+https://cloud.google.com/translate/docs/languages
+`.trim()
+
+    let lang = args[0]
+    let text = args.slice(1).join(' ')
+    if ((args[0] || '').length !== 2) {
+        lang = defaultLang
+        text = args.join(' ')
+    }
+    if (!text && m.quoted && m.quoted.text) text = m.quoted.text
+
+    try {
+       let result = await translate(text, { to: lang, autoCorrect: true }).catch(_ => null) 
+       m.reply(result.text)
+    } catch (e) {
+        throw err
+    } 
+
+}
+handler.help = ['translate <lang> <text>']
+handler.tags = ['tools']
+handler.command = ['translate', 'tl', 'trad', 'tr']
+
 export default handler
